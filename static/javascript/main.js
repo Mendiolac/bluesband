2268d6bff0ffdb99c09deb748da1837a200edb5a
Flamingo <camilledrake95@gmail.com>
2019-03-20 14:43:02 -0500

    updated loading page icon/js still not working

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,8 +1,3 @@
-$(document).ready(function() {
- 
-    setTimeout(function(){
-        $('body').addClass('loaded');
-        $('h1').css('color','#222222');
-    }, 3000);
- 
-});
+window.onload = function() {
+    document.getElementById('body').className = 'loaded';
+  };
